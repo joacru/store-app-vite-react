83d9c2b6a33a0482bb907f00f719d1a7d7039c5e
Joaquín Cruz <joacru1000yugioh@gmail.com>
2024-02-21 12:09:50 -0300

    List of orders

--- src/Components/OrdersCard/index.jsx
+++ src/Components/OrdersCard/index.jsx
@@ -3,11 +3,11 @@ import { ChevronRightIcon } from '@heroicons/react/24/solid'
 function OrdersCard({ order }) {
     return (
         <div className='flex justify-between items-center mb-2 p-2 border border-black rounded-lg w-80'>
-            <p className='flex items-center gap-3'>
-                <span>{ order.date.toLocaleDateString('es-AR') }</span>
-                <span>{ order.totalProducts } items</span>
+            <p className='flex flex-col'>
+                <span className='text-normal font-normal'>{ order.date.toLocaleDateString('es-AR') }</span>
+                <span className='text-sm font-medium'>{ order.totalProducts } articles</span>
             </p>
-            <div className='flex items-center'>
+            <div className='flex items-center gap-2'>
                 <p className='text-lg font-medium'>${ order.totalPrice }</p>
                 <div className='cursor-pointer'>
                     <ChevronRightIcon className='w-4 h-4' />
